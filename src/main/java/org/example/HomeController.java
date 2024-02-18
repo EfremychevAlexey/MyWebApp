35d25a5620cfc7d2f82a2ed2b600ed32e5f029d1
efremychev@bk.ru <efremychev@bk.ru>
2024-02-18 15:05:24 +0300

    first commit

---
+++ src/main/java/org/example/HomeController.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(Model model){
+        return "home";
+    }
+}
