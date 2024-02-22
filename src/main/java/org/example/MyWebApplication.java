48025fc92098ff7e892cc39f43877e5042311e3e
EfremychevAlexey <efremychev@bk.ru>
2024-02-22 15:15:02 +0300

    fix 10

--- src/main/java/org/example/MyWebApplication.java
+++ src/main/java/org/example/MyWebApplication.java
@@ -3,9 +3,17 @@ package org.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MyWebApplication {
+public class MyWebApplication extends SpringBootServletInitializer {
+
+	protected SpringApplicationBuilder
+	configure(SpringApplicationBuilder application){
+		return application.sources(
+				MyWebApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyWebApplication.class, args);
