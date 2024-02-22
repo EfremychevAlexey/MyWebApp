90bbb3af0901b454d6d988d0438d56b078b9e63a
EfremychevAlexey <efremychev@bk.ru>
2024-02-22 15:05:28 +0300

    fix 8

--- src/main/java/org/example/HomeController.java
+++ src/main/java/org/example/HomeController.java
@@ -5,12 +5,13 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HomeController {
 
     @GetMapping("/")
-    public String home(Model model){
-        return "home";
+    public String hello(){
+        return "Hello World!";
     }
 }
